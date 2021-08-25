43641a27c78685644c594ecbaa1d1fb648598173
Everton de Oliveira <84155681+oliveiraeverton@users.noreply.github.com>
2021-08-25 13:16:04 -0400

    Create 1059.cpp

---
+++ 1059.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int i =2;
+    while(i<=100){
+        
+        printf("%d\n", i);
+        i = i + 2;
+    }
+ 
+    return 0;
+}
