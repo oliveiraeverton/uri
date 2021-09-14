3f783ee886db6c8917e190ddd8969444a7ef69c1
Everton de Oliveira <84155681+oliveiraeverton@users.noreply.github.com>
2021-09-14 11:02:43 -0400

    Create 1182.java

---
+++ 1182.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        float[][] matriz;
+        
+        matriz = new float[12][12];
+        char operacao;
+        int coluna;
+        Scanner input = new Scanner(System.in);
+        coluna = input.nextInt();
+        operacao = input.next().charAt(0);;
+        
+        for(int i = 0; i < 12; i++){
+            for(int j = 0; j < 12; j++){
+                matriz[i][j] = input.nextFloat();
+            }
+        }
+        float soma = 0;
+        float media = 0;
+        for(int i = 0; i < 12; i++){
+            soma += matriz[i][coluna];
+        }
+        media = soma/12;
+        
+        if(operacao == 'S'){
+            System.out.printf("%.1f\n", soma);
+        }else{
+            System.out.printf("%.1f\n", media);
+        }
+    }
+ 
+}
