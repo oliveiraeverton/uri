f3e7a65f16ac35b34261ef7c9c4f6123c32a7877
Everton de Oliveira <84155681+oliveiraeverton@users.noreply.github.com>
2021-12-08 14:38:52 -0400

    Create 1115.py

---
+++ 1115.py
@@ -0,0 +1,14 @@
+def DecideQuadrante(x, y):
+  if x > 0 and y > 0:
+    print("primeiro")
+  elif x < 0 and y > 0:
+    print("segundo")
+  elif x < 0 and y < 0:
+    print("terceiro")
+  else:
+    print("quarto")
+x, y = map(int, input().split())
+
+while(x != 0 and y != 0):
+  DecideQuadrante(x, y)
+  x, y = map(int, input().split())
